bdfb9ade5b727692479f0a9910f8e525f8853d58
jernejakrajcar <jerneja.krajcar@scv.si>
2021-01-16 12:44:19 +0100

    error in authorize user

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -1,7 +1,7 @@
 class PinsController < ApplicationController
   before_action :set_pin, only: [:show, :edit, :update, :destroy]
   before_action :authenticate_user!, except: [:show, :index]
-  before_action :authorize_user!, except: [:index, :show]
+  before_action :authorize_user!, except: [:index, :show, :new]
 
   # GET /pins
   # GET /pins.json
