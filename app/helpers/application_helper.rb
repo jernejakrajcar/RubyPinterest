a7cc8c1091aa2f29618e87e543092e71d9249e59
jernejakrajcar <jerneja.krajcar@scv.si>
2021-01-16 14:24:34 +0100

    add user name and edit profile link

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,6 +2,8 @@ module ApplicationHelper
   def display_session_links
     capture do
       if user_signed_in?
+        concat link_to "Edit Profile", edit_user_registration_path
+        concat " "
         concat link_to "Log out", destroy_user_session_path, method: :delete
       else
         concat link_to "Log in", new_user_session_path
