4320620dd65754ebf80c1ab413d5dd00f6ad9a7b
jernejakrajcar <jerneja.krajcar@scv.si>
2021-01-21 22:06:53 +0100

    heroku fix

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -16,8 +16,8 @@ import('js/site')
 const images = require.context('../images', true)
 const imagePath = (name) => images(name, true)
 
-require("masonry/jquery.masonry")
-require("isotope/jquery.isotope")
+//= require masonry/jquery.masonry
+//= require isotope/jquery.isotope
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
