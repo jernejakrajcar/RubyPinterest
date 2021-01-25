367137f7ae62350f4e7346ea0cd6742edac1151b
jernejakrajcar <jerneja.krajcar@scv.si>
2021-01-25 21:36:56 +0100

    env varibales

--- config/initializers/aws.rb
+++ config/initializers/aws.rb
@@ -1,6 +1,7 @@
 Aws.config.update({
   region: 'eu-central-1',
-  credentials: Aws::Credentials.new(:AWS_ACCESS_KEY_ID => ENV['AWS_ACCESS_KEY_ID'], :AWS_SECRET_ACCESS_KEY => ENV['AWS_SECRET_ACCESS_KEY']),
+  credentials: Aws::Credentials.new(ENV['AWS_ACCESS_KEY_ID'], ENV['AWS_SECRET_ACCESS_KEY']),
   })
+
 
 S3_BUCKET = Aws::S3::Resource.new.bucket('rubypinterest-bucket-prod')
