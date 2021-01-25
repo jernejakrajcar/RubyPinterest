b781e557f1e5582c572ebadafc10a122a56f61f6
jernejakrajcar <jerneja.krajcar@scv.si>
2021-01-25 02:37:53 +0100

    images file upload to s3

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -28,6 +28,50 @@ const imagePath = (name) => images(name, true)
 $(function() {
   $('.directUpload').find("input:file").each(function(i, elem) {
     var fileInput = $(elem);
-    console.log(fileInput);
+    var form = $(fileInput.parents('form:first'));
+    var submitButton = form.find('input[type="submit"]');
+    var progressBar = $("<div class='bar'></div>");
+    var barContainer = $("<div class='progress'></div>").append(progressBar);
+    fileInput.after(barContainer);
+    fileInput.fileupload({
+      fileInput: fileInput,
+      url: form.data('url'),
+      type: 'POST',
+      autoUpload: true,
+      formData: form.data('form-data'),
+      paramName: 'file',
+      dataType: 'XML',
+      replaceFileInput: false,
+      progressall: function (e, data) {
+        var progress = parseInt(data.loaded / data.total * 100, 10);
+        progressBar.css('width', progress + '%')
+      },
+      start: function (e) {
+        submitButton.prop('disabled', true);
+
+        progressBar.
+          css('background', 'green').
+          css('display', 'block').
+          css('width', '0%').
+          text("Loading...");
+      },
+      done: function(e, data) {
+        submitButton.prop('disabled', false);
+        progressBar.text("Uploading done");
+
+        var key = $(data.jqXHR.responseXML).find("Key").text();
+        var url = '//' + form.data('host') + '/' + key;
+
+        var input = $("<input />", { type:'hidden', name: fileInput.attr('name'), value: url })
+        form.append(input);
+      },
+      fail: function(e, data) {
+        submitButton.prop('disabled', false);
+
+        progressBar.
+          css("background", "red").
+          text("Failed");
+      }
+    });
   });
 });
