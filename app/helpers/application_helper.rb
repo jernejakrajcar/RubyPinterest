45229e5dca9a2580e1250cc6ef9fecec893b95dc
jernejakrajcar <jerneja.krajcar@scv.si>
2021-01-26 16:48:39 +0100

    navbar css

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,13 +2,13 @@ module ApplicationHelper
   def display_session_links
     capture do
       if user_signed_in?
-        concat link_to "Edit Profile", edit_user_registration_path
+        concat link_to "Edit Profile", edit_user_registration_path, class:"btn btn-outline-secondary"
         concat " "
-        concat link_to "Log out", destroy_user_session_path, method: :delete
+        concat link_to "Log out", destroy_user_session_path, method: :delete, class:"btn btn-outline-danger"
       else
-        concat link_to "Log in", new_user_session_path
+        concat link_to "Log in", new_user_session_path, class:"btn btn-danger"
         concat " "
-        concat link_to "Sign in", new_user_registration_path
+        concat link_to "Sign in", new_user_registration_path, class:"btn btn-danger"
         concat " "
       end
     end
