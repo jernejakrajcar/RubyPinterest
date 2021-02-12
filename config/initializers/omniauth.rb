c61d47214a03ddb5e708c0295a71773da0b0d3b7
jernejakrajcar <jerneja.krajcar@scv.si>
2021-02-12 13:58:42 +0100

    google credentials

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,5 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :google_oauth2, ENV['client_id'], ENV['client_secret']  #, provider_ignores_state: true
+  provider :google_oauth2, ENV['GOOGLE_CLIENT_ID'], ENV['GOOGLE_CLIENT_SECRET']  #, provider_ignores_state: true
 end
 
 # Omniauth.establish_connection!(
