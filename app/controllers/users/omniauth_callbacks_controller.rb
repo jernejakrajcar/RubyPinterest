64223ecace5b2f0b1669f03431e3712e2d1afb85
jernejakrajcar <jerneja.krajcar@scv.si>
2021-02-12 14:54:57 +0100

    maybe?

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,5 +1,6 @@
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
-
+  skip_before_action :protect_from_forgery, raise: false
+  
     def github
       @user = User.from_omniauth(request.env["omniauth.auth"])
       if @user.persisted?
