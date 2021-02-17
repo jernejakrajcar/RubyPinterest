1f817ccb840bd60fadcb962ebef22f3626886857
jernejakrajcar <jerneja.krajcar@scv.si>
2021-02-17 10:03:53 +0100

    added boards

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   has_many :pins, dependent: :destroy
+  has_many :boards
   has_one_attached :avatar
   acts_as_commontator
 
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
    mount Commontator::Engine => '/commontator'
 
   resources :pins
+
+  resources :boards
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'pins#index'
 end
