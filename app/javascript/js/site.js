cb00fd18bf30c05bb73de560d0cb01d4c05cf348
jernejakrajcar <jerneja.krajcar@scv.si>
2021-02-20 22:52:53 +0100

    changed application.js

--- app/javascript/js/site.js
+++ app/javascript/js/site.js
@@ -1,12 +1,53 @@
 import 'bootstrap';
 import "@fortawesome/fontawesome-free/js/all";
 
-$(function(){
+$(window).on('load', function() {
+  $('.directUpload').find("input:file").each(function(i, elem) {
+    var fileInput = $(elem);
+    var form = $(fileInput.parents('form:first'));
+    var submitButton = form.find('input[type="submit"]');
+    var progressBar = $("<div class='bar'></div>");
+    var barContainer = $("<div class='progress'></div>").append(progressBar);
+    fileInput.after(barContainer);
+    fileInput.fileupload({
+      fileInput: fileInput,
+      url: form.data('url'),
+      type: 'POST',
+      autoUpload: true,
+      formData: form.data('form-data'),
+      paramName: 'file',
+      dataType: 'XML',
+      replaceFileInput: false,
+      progressall: function (e, data) {
+        var progress = parseInt(data.loaded / data.total * 100, 10);
+        progressBar.css('width', progress + '%')
+      },
+      start: function (e) {
+        submitButton.prop('disabled', true);
 
-  // $('#pins').masonry({
-  //   itemSelector: '.box',
-  //   columnWidth: 100,
-  //   gutterWidth: 40
-  // });
+        progressBar.
+           css('background', 'green').
+           css('display', 'block').
+           css('width', '0%').
+           text("Loading...");
+       },
+         done: function(e, data) {
+           submitButton.prop('disabled', false);
+           progressBar.text("Uploading done");
 
+           var key = $(data.jqXHR.responseXML).find("Key").text();
+           var url = '//' + form.data('host') + '/' + key;
+
+           var input = $("<input />", { type:'hidden', name: fileInput.attr('name'), value: url })
+           form.append(input);
+         },
+         fail: function(e, data) {
+           submitButton.prop('disabled', false);
+
+           progressBar.
+             css("background", "red").
+             text("Failed");
+         }
+    });
+  });
 });
