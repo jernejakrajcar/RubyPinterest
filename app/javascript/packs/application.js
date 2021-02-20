3e89f0fb1505af9e9a7aeaa5e2bee6e805bf2dd9
jernejakrajcar <jerneja.krajcar@scv.si>
2021-02-20 23:04:53 +0100

    added require_tree

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,9 +8,9 @@ require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 require("jquery")
+//= require_tree .
 //= require jquery.ui.widget
 //= require jquery-fileupload/basic
-
 //= require z.jquery.fileupload
 
 //SCSS
@@ -28,8 +28,3 @@ const imagePath = (name) => images(name, true)
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
-
-//
-//     });
-//   });
-// });
