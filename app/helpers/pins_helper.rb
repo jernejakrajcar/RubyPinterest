929febcbadd402946e1261be77bf1904942f9880
jernejakrajcar <jerneja.krajcar@scv.si>
2021-02-21 00:49:44 +0100

    style #2

--- app/helpers/pins_helper.rb
+++ app/helpers/pins_helper.rb
@@ -13,9 +13,10 @@ module PinsHelper
     capture do
       if current_user == pin.user
         concat link_to 'Edit', edit_pin_path(pin), class:"btn btn-outline-danger"
-        concat ' | '
+        concat '  '
         concat link_to 'Destroy', pin, method: :delete, data: { confirm: 'Are you sure? '}, class:"btn btn-danger"
-        concat ' | '
+        concat '  '
+        concat link_to 'Back', pins_path, class:"btn btn-secondary"
       else
         concat link_to 'Back', pins_path, class:"btn btn-secondary"
       end
