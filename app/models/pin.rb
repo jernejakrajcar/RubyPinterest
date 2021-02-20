8a2805fcfe8e0a6c4f221b220ec28e672a3587ea
jernejakrajcar <jerneja.krajcar@scv.si>
2021-02-20 23:47:32 +0100

    Revert "photo upload for pin"
    This reverts commit 313dc4e43ecc5739105fcef4257ac492df37e86e.

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -1,9 +1,8 @@
 class Pin < ApplicationRecord
   belongs_to :user
-
+  belongs_to :board
   has_one_attached :image
 
-  belongs_to :board
   acts_as_commontable dependent: :destroy
   validates :title, presence: true
   validates :description, presence: true
