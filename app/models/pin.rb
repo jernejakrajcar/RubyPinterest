bf48d932c779954c1ab8fb84c6f59e5684f66f04
jernejakrajcar <jerneja.krajcar@scv.si>
2021-02-21 02:58:38 +0100

    search pins

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -5,4 +5,18 @@ class Pin < ApplicationRecord
   acts_as_commontable dependent: :destroy
   validates :title, presence: true
   validates :description, presence: true
+
+  def self.search(search)
+    if search
+      pin = Pin.find_by(title: search)
+      if pin
+        self.where(["title LIKE ?","%${search}%"])
+      else
+        @pins = Pin.all
+      end
+    else
+      @pins = Pin.all
+    end
+  end
+
 end
